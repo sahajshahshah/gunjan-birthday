59048cb1fa5961f063fdc954823b87787cb130ca
sahajshahshah <sahajshah33@gmail.com>
2026-07-30 22:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1188,3 +1188,22 @@ if (kitty) {
     });
 
 }
+
+kitty.addEventListener("click", function(e){
+
+    const heart = document.createElement("div");
+
+    heart.className = "floating-heart";
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = e.clientX + "px";
+    heart.style.top = e.clientY + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    },1500);
+
+});
